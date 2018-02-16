f645c6a998ac217893db19328b35c6eb801fb6e9
komodochristopher <36484134+komodochristopher@users.noreply.github.com>
2018-02-16 14:55:32 -0800

    Add files via upload

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,15 +1,16 @@
 package section2;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot bob = new Robot();
+		bob.penDown();
+		bob.setSpeed(500);
+		bob.move(200);
 	}
+
 }
